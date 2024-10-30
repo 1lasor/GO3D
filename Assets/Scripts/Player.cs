ca7bea79714067c6e168f407e0f8170940ae6c9c
1lasor <2244602447@qq.com>
2024-10-30 10:18:24 +0800

    添加场景保存文件

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,7 +25,7 @@ public class Player : MonoBehaviour
                 // 检查是否同时按下了Control键
                 if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))//Ctrl+左键显示平面
                 {
-                    ShowPlaneAt(position);
+                    ShowPlaneAt(position,0);
                 }
                 else//左键单击落子
                 {
@@ -36,21 +36,9 @@ public class Player : MonoBehaviour
         }
     }
 
-    void ShowPlaneAt(Vector3 position)
+    void ShowPlaneAt(Vector3 position,int axis)
     {
-        // 根据x, y, z调整摄像机或隐藏/显示平面
-        // 例如，如果你想显示x平面，可以设置y和z坐标相同
-        Camera.main.transform.position = new Vector3(
-            Camera.main.transform.position.x,
-            position.y,
-            position.z
-        );
-
-        // 确保摄像机朝向正确的方向，比如朝向原点
-        Camera.main.transform.LookAt(position);
-        Camera.main.enabled = true;
-
-        // 这里可以添加代码来隐藏或显示其他平面
+        
     }
 
     void PlacePiece(Vector3Int position)
